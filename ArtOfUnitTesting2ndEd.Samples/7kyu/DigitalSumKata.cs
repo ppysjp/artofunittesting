3c2fa1172a3230c3463b8fa7dc7f16bad4cb1886
Samuel Palmer <Samuel.Palmer@dpr.co.uk>
2022-01-10 12:30:24 +0000

    Reorganised DigitalSum Kata

--- ArtOfUnitTesting2ndEd.Samples/7kyu/DigitalSumKata.cs
+++ ArtOfUnitTesting2ndEd.Samples/7kyu/DigitalSumKata.cs
@@ -7,10 +7,10 @@ namespace _7kyu
 {
     public class DigitalSumKata
     {
-        private static bool Between0And10(long n) => 0 < n && n < 10;
-        private static long DivideBy10(long n) => n / 10;
-        public static long LastDigit(long n) => n % 10;
-        public static long SumDigits(long n) => ApplyFunctionToNumber.SumNPFI(n, Between0And10, LastDigit, DivideBy10);
         public static long DigitalSum(long n) => ApplyFunctionToNumber.ComposeNPF(n, Between0And10, SumDigits);
+        public static long SumDigits(long n) => ApplyFunctionToNumber.SumNPFI(n, Between0And10, LastDigit, DivideBy10);
+        private static bool Between0And10(long n) => 0 < n && n < 10;
+        public static long LastDigit(long n) => n % 10;
+        private static long DivideBy10(long n) => n / 10;
     }
 }
