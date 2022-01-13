00dd84943a3836c8bccff9a27cc0fe5be5175e72
Samuel Palmer <Samuel.Palmer@dpr.co.uk>
2022-01-13 16:31:37 +0000

    First video from Linq tutorial

---
+++ ArtOfUnitTesting2ndEd.Samples/7kyu.Tests/BinaryAdditionTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace _7kyu.Tests
+{
+    [TestFixture]
+    public class AddBinaryTest
+    {
+        [Test]
+        public void AddBinary()
+        {
+            Assert.AreEqual("11", BinaryAdditionKata.AddBinary(1, 2), "Should return \"11\" for 1 + 2");
+        }
+
+        [TestCase(0,0)]
+        [TestCase(1, 1)]
+        [TestCase(2, 10)]
+        [TestCase(3, 11)]
+        [TestCase(85, 1010101)]
+        public void IntToBinary(int a, int result)
+        {
+            Assert.AreEqual(result, BinaryAdditionKata.IntToBinary(a));
+        }
+
+    }
+}
---
+++ ArtOfUnitTesting2ndEd.Samples/7kyu/BinaryAdditionKata.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _7kyu
+{
+    public class BinaryAdditionKata
+    {
+        public static string AddBinary(int a, int b)
+        {
+            return Convert.ToString(a + b, 2); ;
+           // return AddBinaryInt(a, b).ToString();
+        }
+
+        private static long AddBinaryInt(int a, int b)
+        {
+            return IntToBinary(a + b);
+          
+        }      
+
+        public static int IntToBinary(int a)        
+        {
+            if (a == 1 | a == 0)
+            {
+                return a;
+            }
+            else if (a % 2 == 0)
+            {
+                return 10 * IntToBinary(a / 2);
+            }
+            else
+            {
+                return 1 + IntToBinary(a - 1);
+            }
+        }
+    }
+}
